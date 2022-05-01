aa77ef622b6075a4e15d6bef2ae581692bcc9bcb
Neil Bennett <neil.bennett876@Neils-MacBook-Air.local>
2022-05-01 09:45:11 -0400

    initial commit

---
+++ connectDb.js
@@ -0,0 +1,9 @@
+import admin from "firebase-admin";
+import service_account from "./credentials.js";
+
+admin.initializeApp({
+    credential: admin.credential.cert(service_account)
+})
+
+const db = admin.firestore();
+export const newColl = db.collection("League");
